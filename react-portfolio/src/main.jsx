3e10636eae9a6ac3d8745f503a2342469140b2fe
nsetty <nsett7388@gmail.com>
2024-03-11 18:16:59 -0400

    home css and main jsx changes

--- react-portfolio/src/main.jsx
+++ react-portfolio/src/main.jsx
@@ -1,17 +1,15 @@
-import ReactDOM from 'react-dom/client'
-// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from './App';
-import Error from './pages/Error';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
+import App from "./App";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
 
-// Define the accessible routes, and which components respond to which URL
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <Error />,
     children: [
@@ -20,17 +18,17 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: '/About',
+        path: "/About",
         element: <About />,
       },
       {
-        path: '/Projects',
+        path: "/Projects",
         element: <Projects />,
       },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 );
